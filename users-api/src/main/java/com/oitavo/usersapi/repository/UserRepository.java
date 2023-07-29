3d57dc289760d25230dd648569a07ea30ab4a146
Otávio Landim <otavio.landim@gmail.com>
2023-07-29 12:22:35 -0300

    Added user repository

---
+++ users-api/src/main/java/com/oitavo/usersapi/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.oitavo.usersapi.repository;
+
+import com.oitavo.usersapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByCpf(String cpf);
+
+    List<User> queryByNameLike(String name);
+
+}
