82e404e1387810ea3b4be78044986d443ce0938b
Otávio Landim <otavio.landim@gmail.com>
2023-07-29 09:08:35 -0300

    Renamed attributes and methods to user dto

--- users-api/src/main/java/com/oitavo/usersapi/dto/UserDTO.java
+++ users-api/src/main/java/com/oitavo/usersapi/dto/UserDTO.java
@@ -6,56 +6,56 @@ import java.util.Date;
 import java.util.List;
 
 public class UserDTO {
-    private  String nome;
-    private  String cpf;
-    private  String endereco;
-    private  String email;
-    private  String telefone;
-    private  Date dataCadastro;
+    private String nome;
+    private String cpf;
+    private String address;
+    private String email;
+    private String telephone;
+    private Date dateRegister;
 
-    public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
+    //public static List<UserDTO> users = new ArrayList<UserDTO>();
 
     public UserDTO () {}
 
-    public UserDTO (String nome, String cpf, String endereco, String email, String telefone, Date dataCadastro) {
+    public UserDTO (String nome, String cpf, String address, String email, String telephone, Date dateRegister) {
         this.nome = nome;
         this.cpf = cpf;
-        this.endereco = endereco;
+        this.address = address;
         this.email = email;
-        this.telefone = telefone;
-        this.dataCadastro = dataCadastro;
+        this.telephone = telephone;
+        this.dateRegister = dateRegister;
     }
 
-    @PostConstruct
-    public void initiateList(){
-        UserDTO userDTO = new UserDTO();
-        userDTO.setNome("Otavio");
-        userDTO.setCpf("123");
-        userDTO.setEndereco("Rua A");
-        userDTO.setEmail("teste_otavio@bla.com");
-        userDTO.setTelefone("1232-3232");
-        userDTO.setDataCadastro(new Date());
-
-        UserDTO userDTO2 = new UserDTO();
-        userDTO.setNome("Erika");
-        userDTO.setCpf("123");
-        userDTO.setEndereco("Rua A");
-        userDTO.setEmail("teste_otavio@bla.com");
-        userDTO.setTelefone("1232-3232");
-        userDTO.setDataCadastro(new Date());
-
-        UserDTO userDTO3 = new UserDTO();
-        userDTO.setNome("Isaac");
-        userDTO.setCpf("123");
-        userDTO.setEndereco("Rua A");
-        userDTO.setEmail("teste_otavio@bla.com");
-        userDTO.setTelefone("1232-3232");
-        userDTO.setDataCadastro(new Date());
-
-        usuarios.add(userDTO);
-        usuarios.add(userDTO2);
-        usuarios.add(userDTO3);
-    }
+//    @PostConstruct
+//    public void initiateList(){
+//        UserDTO userDTO = new UserDTO();
+//        userDTO.setNome("Otavio");
+//        userDTO.setCpf("123");
+//        userDTO.setAddress("Rua A");
+//        userDTO.setEmail("teste_otavio@bla.com");
+//        userDTO.setTelephone("1232-3232");
+//        userDTO.setDateRegister(new Date());
+//
+//        UserDTO userDTO2 = new UserDTO();
+//        userDTO2.setNome("Erika");
+//        userDTO2.setCpf("456");
+//        userDTO2.setAddress("Rua B");
+//        userDTO2.setEmail("erika@bla.com");
+//        userDTO2.setTelephone("1232-3232");
+//        userDTO2.setDateRegister(new Date());
+//
+//        UserDTO userDTO3 = new UserDTO();
+//        userDTO3.setNome("Isaac");
+//        userDTO3.setCpf("789");
+//        userDTO3.setAddress("Rua C");
+//        userDTO3.setEmail("isaac@bla.com");
+//        userDTO3.setTelephone("1232-3232");
+//        userDTO3.setDateRegister(new Date());
+//
+//        users.add(userDTO);
+//        users.add(userDTO2);
+//        users.add(userDTO3);
+//    }
 
     public String getNome () {
         return nome;
@@ -73,12 +73,12 @@ public class UserDTO {
         this.cpf = cpf;
     }
 
-    public String getEndereco () {
-        return endereco;
+    public String getAddress () {
+        return address;
     }
 
-    public void setEndereco (String endereco) {
-        this.endereco = endereco;
+    public void setAddress (String address) {
+        this.address = address;
     }
 
     public String getEmail () {
@@ -89,19 +89,34 @@ public class UserDTO {
         this.email = email;
     }
 
-    public String getTelefone () {
-        return telefone;
+    public String getTelephone () {
+        return telephone;
     }
 
-    public void setTelefone (String telefone) {
-        this.telefone = telefone;
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
-    public Date getDataCadastro () {
-        return dataCadastro;
+    public Date getDateRegister () {
+        return dateRegister;
     }
 
-    public void setDataCadastro (Date dataCadastro) {
-        this.dataCadastro = dataCadastro;
+    public void setDateRegister (Date dateRegister) {
+        this.dateRegister = dateRegister;
+    }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserDTO userDTO = (UserDTO) o;
+
+        return cpf.equals(userDTO.cpf);
+    }
+
+    @Override
+    public int hashCode () {
+        return cpf.hashCode();
     }
 }
