5c0dddd9d92a1d036a4514bb29a7d805d975b97a
Otávio Landim <otavio.landim@gmail.com>
2023-08-16 17:58:51 -0300

    Adding script migration to users

---
+++ users-api/src/main/resources/db/migrations/V1_create_user_table.sql
@@ -0,0 +1,11 @@
+create schema if not exists users;
+
+create table users.user (
+    id big-serial primary key,
+    name varchar(100) not null,
+    cpf varchar(100) not null,
+    address varchar(100) not null,
+    email varchar(100) not null,
+    telephone varchar(100) not null,
+    date_register timestamp not null,
+);
